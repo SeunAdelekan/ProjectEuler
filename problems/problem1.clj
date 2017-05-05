638008d431d8f8319ac3508e201488a2d098d774
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-05-06 00:17:33 +0100

    Added solution for problem 1

---
+++ problems/problem1.clj
@@ -0,0 +1,26 @@
+(ns euler.core
+  (:gen-class))
+
+(defn sum-multiples
+  "Finds the sum of multiples of two numeric vars
+   x and y that are less than var ceil."
+  [x y ceil]
+  (def multiple-sum 0)
+  (def current-val 0)
+  (while (< current-val ceil)
+    (do
+      (if
+        (or
+          (== 0
+              (rem current-val x))
+          (== 0
+              (rem current-val y)))
+        (def multiple-sum
+          (+ multiple-sum current-val)))
+      (def current-val
+                      (inc current-val))))
+  (println multiple-sum))
+
+(defn -main
+  [& args]
+  (sum-multiples 3 5 1000))
